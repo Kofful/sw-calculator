1437fbd93e958a753597afc5342b5d258d59adff
Eugene Danileichenko <evgeniy.d48@gmail.com>
2022-11-29 20:10:10 +0200

    Resolve #20

--- src/main/java/khai/calculator/operations/SubtractInterface.java
+++ src/main/java/khai/calculator/operations/SubtractInterface.java
@@ -4,9 +4,11 @@ public interface SubtractInterface {
     /**
      * Subtracts the subtracted number from the reduced number
      *
-     * @param reduced reduced number
-     * @param subtracted subtracted number
      * @return difference
      */
-    double subtract(double reduced, double subtracted);
+    double subtract();
+    void setReduced(double reduced);
+    double getReduced();
+    void setSubtracted(double subtracted);
+    double getSubtracted();
 }
--- src/main/java/khai/calculator/services/DifferenceOfTwoNumbs.java
+++ src/main/java/khai/calculator/services/DifferenceOfTwoNumbs.java
@@ -3,15 +3,37 @@ package khai.calculator.services;
 import khai.calculator.operations.SubtractInterface;
 
 public class DifferenceOfTwoNumbs implements SubtractInterface {
+    private double reduced;
+    private double subtracted;
     /**
      * Subtracts the subtracted number from the reduced number
      *
-     * @param reduced    reduced number
-     * @param subtracted subtracted number
      * @return difference
      */
     @Override
-    public double subtract(double reduced, double subtracted) {
-        return reduced - subtracted;
+    public double subtract() {
+        return this.reduced - this.subtracted;
     }
+
+    @Override
+    public void setReduced(double reduced) {
+        this.reduced = reduced;
+    }
+
+    @Override
+    public double getReduced() {
+        return this.reduced;
+    }
+
+    @Override
+    public void setSubtracted(double subtracted) {
+        this.subtracted = subtracted;
+    }
+
+    @Override
+    public double getSubtracted() {
+        return this.subtracted;
+    }
+
+
 }
