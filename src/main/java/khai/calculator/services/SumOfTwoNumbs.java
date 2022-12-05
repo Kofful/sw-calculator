65ee9ebf19d447d9dab6cfca097767823739524a
egor.sumskoy <segoruch@gmail.com>
2022-12-05 23:01:47 +0200

    added and realised accessors for sum operation

--- src/main/java/khai/calculator/services/SumOfTwoNumbs.java
+++ src/main/java/khai/calculator/services/SumOfTwoNumbs.java
@@ -3,6 +3,11 @@ package khai.calculator.services;
 import khai.calculator.operations.SumInterface;
 
 public class SumOfTwoNumbs implements SumInterface {
+
+    private double firstAddition;
+
+    private double secondAddition;
+
     /**
      * Counts sum of two additions
      *
@@ -13,5 +18,25 @@ public class SumOfTwoNumbs implements SumInterface {
     @Override
     public double sum(double firstAddition, double secondAddition) {
         return firstAddition + secondAddition;
+    }
+
+    @Override
+    public void setFirstAddition(double firstAddition) {
+        this.firstAddition = firstAddition;
+    }
+
+    @Override
+    public void setSecondAddition(double secondAddition) {
+        this.secondAddition = secondAddition;
+    }
+
+    @Override
+    public double getFirstAddition() {
+        return firstAddition;
+    }
+
+    @Override
+    public double getSecondAddition() {
+        return secondAddition;
     }
 }
